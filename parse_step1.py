50f4f912e63dab86be4757a2dc748d8c7a4e0085
Simon Pirschel <sp@abusix.com>
2014-04-22 19:53:10 +0200

    PEP-8 code style

--- parse_step1.py
+++ parse_step1.py
@@ -1,35 +1,39 @@
 import urllib
 from bs4 import BeautifulSoup
 
-URL = "http://haunebu.raxys.net:5000/?p="
+
+URL = 'http://haunebu.raxys.net:5000/?p='
+
 
 if __name__ == '__main__':
     for x in range(1, 11):
         url_str = '%s%d' % (URL, x)
         sock = urllib.urlopen(url_str)
-
-        htmlSource = sock.read()
+        html_source = sock.read()
         sock.close()
-        soup = BeautifulSoup(htmlSource)
-        #print htmlSource
+
+        soup = BeautifulSoup(html_source)
+        #print html_source
         table = soup.find('table', attrs={'class': 'table'})
 
-        headings = [th.get_text() for th in table.find("tr").find_all("th")]
+        headings = [th.get_text() for th in table.find('tr').find_all('th')]
 
         datasets = []
 
-        for row in table.findAll("tr")[1:]:
-            dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
+        for row in table.findAll('tr')[1:]:
+            dataset = zip(
+                headings, (td.get_text() for td in row.find_all('td'))
+            )
             datasets.append(dataset)
 
         #print datasets
 
         for dataset in datasets:
             for field in dataset:
-                print "{0:<16}: {1}".format(field[0], field[1])
+                print '{0:<16}: {1}'.format(field[0], field[1])
 
     #print soup('table')[0].prettify()
 
-    #    for cell in row.findAll("td"):
+    #    for cell in row.findAll('td'):
     #        dataset = cell.findAll(text=True)
     #        print dataset
