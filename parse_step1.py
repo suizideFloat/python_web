f0ef6d1658fc9a6630d38366ea1a27a58c0d6ff5
Simon Pirschel <sp@abusix.com>
2014-04-22 19:47:40 +0200

    proper style, global var upper case. if __name__ == '__main__' to avoid issues if other code imports from this file

--- parse_step1.py
+++ parse_step1.py
@@ -1,35 +1,35 @@
 import urllib
 from bs4 import BeautifulSoup
 
-url = "http://haunebu.raxys.net:5000/?p="
-for x in range(1, 11):
-    url_str = url + str(x)
-    sock = urllib.urlopen(url_str)
-    x += 1
-    htmlSource = sock.read()
-    sock.close()
-    soup = BeautifulSoup(htmlSource)
-#print htmlSource
-    table = soup.find('table', attrs={'class': 'table'})
+URL = "http://haunebu.raxys.net:5000/?p="
 
-    headings = [th.get_text() for th in table.find("tr").find_all("th")]
+if __name__ == '__main__':
+    for x in range(1, 11):
+        url_str = URL + str(x)
+        sock = urllib.urlopen(url_str)
+        x += 1
+        htmlSource = sock.read()
+        sock.close()
+        soup = BeautifulSoup(htmlSource)
+        #print htmlSource
+        table = soup.find('table', attrs={'class': 'table'})
 
-    datasets = []
+        headings = [th.get_text() for th in table.find("tr").find_all("th")]
 
+        datasets = []
 
+        for row in table.findAll("tr")[1:]:
+            dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
+            datasets.append(dataset)
 
-    for row in table.findAll("tr")[1:]:
-        dataset = zip(headings, (td.get_text() for td in row.find_all("td")))
-        datasets.append(dataset)
+        #print datasets
 
-#print datasets
+        for dataset in datasets:
+            for field in dataset:
+                print "{0:<16}: {1}".format(field[0], field[1])
 
-    for dataset in datasets:
-        for field in dataset:
-            print "{0:<16}: {1}".format(field[0], field[1])
+    #print soup('table')[0].prettify()
 
-#print soup('table')[0].prettify()
-
-#    for cell in row.findAll("td"):
-#        dataset = cell.findAll(text=True)
-#        print dataset
+    #    for cell in row.findAll("td"):
+    #        dataset = cell.findAll(text=True)
+    #        print dataset
